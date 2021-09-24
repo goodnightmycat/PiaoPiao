0f996104b239d50d306986e5fcf5a9a64db356cd
fanxiaoyang <15816221326@163.com>
2021-09-24 21:43:12 +0800

    UI修改,界面完善

--- app/src/main/java/com/example/piaopiao/MainActivity.kt
+++ app/src/main/java/com/example/piaopiao/MainActivity.kt
@@ -20,14 +20,19 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bubbleContainView: RelativeLayout
-    private lateinit var testTv: TextView
+    private lateinit var dayTv: TextView
 
-    private val stringList = arrayOf("哈哈", "嘿嘿", "77", "88")
+    private val stringList =
+        arrayOf("预备备", "嘿嘿", "可爱死咯", "不可以吗", "高贵得很", "等下下", "你要道理还是要女票", "哼", "坏蛋","男票","阿票","票票","想男票了","男票困困","男票抱抱",
+            "男票亲亲","吃饱饱","分什么你我","男票～我好喜欢你呀","早安男票","男票我醒了","男票我想你了","我最～喜欢男票了","晚安男票","男票亲我")
 
     /**
      * 2时间戳
      */
     private val startTime = 1628870400000
+
+    var colorBlue: Int = 0
+    var colorPink: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,18 +41,26 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         StatusBarHelper.setStatusBarHint(this, Color.parseColor("#f3f3f3f3"), true)
         bubbleContainView = findViewById(R.id.rootContainer)
-    }
-
-    private fun randomLayoutParams(): RelativeLayout.LayoutParams {
-        val layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
-        layoutParams.addRule(ALIGN_PARENT_END)
-        layoutParams.topMargin = randomY()
-        return layoutParams
+        dayTv = findViewById(R.id.tv_days)
+        colorBlue = this.resources.getColor(R.color.my_blue)
+        colorPink = this.resources.getColor(R.color.my_pink)
+        initView()
     }
 
     override fun onResume() {
         super.onResume()
-        initView()
+        val days = (System.currentTimeMillis() - startTime)/(1000*60*60*24)
+        dayTv.text="$days"
+    }
+
+    private fun randomLayoutParams(): RelativeLayout.LayoutParams {
+        val layoutParams = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.WRAP_CONTENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.addRule(ALIGN_PARENT_END)
+        layoutParams.topMargin = randomY()
+        return layoutParams
     }
 
     private fun randomY(): Int {
@@ -61,6 +74,7 @@ class MainActivity : AppCompatActivity() {
                 repeat(stringList.size) {
                     delay(2000)
                     val textView = TextView(this@MainActivity)
+                    textView.setTextColor(randomColor())
                     textView.text = stringList[it]
                     startBubble(textView)
                 }
@@ -69,10 +83,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun randomColor(): Int {
+        val int = Random(System.currentTimeMillis()).nextInt()
+        return if (int % 2 == 0) {
+            colorBlue
+        } else {
+            colorPink
+        }
+    }
+
     private fun startBubble(view: TextView) {
         bubbleContainView.addView(view, randomLayoutParams())
         val translationX = getScreenWidth(this)
-        val animator = ObjectAnimator.ofFloat(view, "translationX", 0f, (0f - translationX))
+        val animator =
+            ObjectAnimator.ofFloat(view, "translationX", 0f + translationX / 2, (0f - translationX))
         animator.duration = 5000
         animator.interpolator = LinearInterpolator()
         animator.addListener(object : Animator.AnimatorListener {
